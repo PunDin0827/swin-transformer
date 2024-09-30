411225d5762a2047a7df78a9bbb361d744a1fa35
PunDin0827 <andy14x@gmail.com>
2024-09-30 20:45:24 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -16,7 +16,7 @@ class MyDataSet(Dataset):
 
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
-        # RGB為彩色圖片，L為灰度圖片
+        # RGB為彩色圖片，L為灰階圖片
         if img.mode != 'RGB':
             raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
         label = self.images_class[item]
@@ -28,7 +28,7 @@ class MyDataSet(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        # 官方實現的default_collate可以参考
+        # 官方的default_collate
         # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
         images, labels = tuple(zip(*batch))
 
